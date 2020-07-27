cfd2b52512ce807af2713dba3f553e6ff0ef6cb4
BodoqueChupeteSuazo <mariano.vinuela@virginiogomez.cl>
2020-07-27 12:23:31 -0400

    Hola

--- src/basedatos/Conexion.java
+++ src/basedatos/Conexion.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+//Hola
 /**
  * Clase para conectarse a una base de datos con cambios
  * @author Vi
